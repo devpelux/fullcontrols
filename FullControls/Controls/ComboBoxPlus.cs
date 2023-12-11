89a9e2c95ed9ac2dd02c76534acce760db57f061
devpelux <devpelux@outlook.com>
2023-12-11 15:18:26 +0100

    Added label in ComboBoxPlus.

--- FullControls/Controls/ComboBoxPlus.cs
+++ FullControls/Controls/ComboBoxPlus.cs
@@ -1,4 +1,5 @@
-﻿using FullControls.Core;
+﻿using FullControls.Common;
+using FullControls.Core;
 using System;
 using System.ComponentModel;
 using System.Windows;
@@ -981,6 +982,384 @@ namespace FullControls.Controls
         public static readonly DependencyProperty TextContextMenuProperty =
             DependencyProperty.Register(nameof(TextContextMenu), typeof(ContextMenu), typeof(ComboBoxPlus),
                 new PropertyMetadata(null));
+
+        #endregion
+
+        #region Label
+
+        /// <summary>
+        /// Gets or sets the label displayed alongside the control.
+        /// </summary>
+        public object Label
+        {
+            get => GetValue(LabelProperty);
+            set => SetValue(LabelProperty, value);
+        }
+
+        /// <summary>
+        /// Identifies the <see cref="Label"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty LabelProperty =
+            DependencyProperty.Register(nameof(Label), typeof(object), typeof(ComboBoxPlus),
+                new FrameworkPropertyMetadata(null, new PropertyChangedCallback((d, e)
+                    => d.SetValue(LabelTypePropertyKey, CurrentLabelType(d)))));
+
+        /// <summary>
+        /// Gets or sets the icon displayed alongside the control.
+        /// </summary>
+        /// <remarks>The icon will be displayed only if <see cref="Label"/> is <see langword="null"/>.</remarks>
+        public ImageSource Icon
+        {
+            get => (ImageSource)GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
+        }
+
+        /// <summary>
+        /// Identifies the <see cref="Icon"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register(nameof(Icon), typeof(ImageSource), typeof(ComboBoxPlus),
+                new FrameworkPropertyMetadata(null, new PropertyChangedCallback((d, e)
+                    => d.SetValue(LabelTypePropertyKey, CurrentLabelType(d)))));
+
+        /// <summary>
+        /// Gets a value indicating the actual label type.
+        /// </summary>
+        public LabelType LabelType => (LabelType)GetValue(LabelTypeProperty);
+
+        #region LabelTypeProperty
+
+        /// <summary>
+        /// The <see cref="DependencyPropertyKey"/> for <see cref="LabelType"/> dependency property.
+        /// </summary>
+        private static readonly DependencyPropertyKey LabelTypePropertyKey =
+            DependencyProperty.RegisterReadOnly(nameof(LabelType), typeof(LabelType), typeof(ComboBoxPlus),
+                new FrameworkPropertyMetadata(LabelType.Undefined));
+
+        /// <summary>
+        /// Identifies the <see cref="LabelType"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty LabelTypeProperty = LabelTypePropertyKey.DependencyProperty;
+
+        /// <summary>
+        /// Calculates the current label type based on the values of <see cref="Label"/> and <see cref="Icon"/>.
+        /// </summary>
+        private static LabelType CurrentLabelType(DependencyObject d)
+        {
+            bool label = d.GetValue(LabelProperty) != null;
+            bool icon = d.GetValue(IconProperty) != null;
+
+            //Visualization predominance: Label -> Icon -> Nothing
+            return label ? LabelType.Content : icon ? LabelType.Icon : LabelType.Undefined;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Gets or sets the placement of the label.
+        /// </summary>
+        public Dock LabelPlacement
+        {
+            get => (Dock)GetValue(LabelPlacementProperty);
+            set => SetValue(LabelPlacementProperty, value);
+        }
+
+        /// <summary>
+        /// Identifies the <see cref="LabelPlacement"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty LabelPlacementProperty =
+            DependencyProperty.Register(nameof(LabelPlacement), typeof(Dock), typeof(ComboBoxPlus),
+                new PropertyMetadata(Dock.Top));
+
+        /// <summary>
+        /// Gets or sets the width of the label.
+        /// </summary>
+        [TypeConverter(typeof(LengthConverter))]
+        [Localizability(LocalizationCategory.None, Readability = Readability.Unreadable)]
+        public double LabelWidth
+        {
+            get => (double)GetValue(LabelWidthProperty);
+            set => SetValue(LabelWidthProperty, value);
+        }
+
+        /// <summary>
+        /// Identifies the <see cref="LabelWidth"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty LabelWidthProperty =
+            DependencyProperty.Register(nameof(LabelWidth), typeof(double), typeof(ComboBoxPlus),
+                new FrameworkPropertyMetadata(double.NaN),
+                new ValidateValueCallback(Util.IsWidthHeightValid));
+
+        /// <summary>
+        /// Gets or sets the height of the label.
+        /// </summary>
+        [TypeConverter(typeof(LengthConverter))]
+        [Localizability(LocalizationCategory.None, Readability = Readability.Unreadable)]
+        public double LabelHeight
+        {
+            get => (double)GetValue(LabelHeightProperty);
+            set => SetValue(LabelHeightProperty, value);
+        }
+
+        /// <summary>
+        /// Identifies the <see cref="LabelHeight"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty LabelHeightProperty =
+            DependencyProperty.Register(nameof(LabelHeight), typeof(double), typeof(ComboBoxPlus),
+                new FrameworkPropertyMetadata(double.NaN),
+                new ValidateValueCallback(Util.IsWidthHeightValid));
+
+        /// <summary>
+        /// Gets or sets the max width of the label.
+        /// </summary>
+        [TypeConverter(typeof(LengthConverter))]
+        [Localizability(LocalizationCategory.None, Readability = Readability.Unreadable)]
+        public double LabelMaxWidth
+        {
+            get => (double)GetValue(LabelMaxWidthProperty);
+            set => SetValue(LabelMaxWidthProperty, value);
+        }
+
+        /// <summary>
+        /// Identifies the <see cref="LabelMaxWidth"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty LabelMaxWidthProperty =
+            DependencyProperty.Register(nameof(LabelMaxWidth), typeof(double), typeof(ComboBoxPlus),
+                new FrameworkPropertyMetadata(double.PositiveInfinity),
+                new ValidateValueCallback(Util.IsMaxWidthHeightValid));
+
+        /// <summary>
+        /// Gets or sets the max height of the label.
+        /// </summary>
+        [TypeConverter(typeof(LengthConverter))]
+        [Localizability(LocalizationCategory.None, Readability = Readability.Unreadable)]
+        public double LabelMaxHeight
+        {
+            get => (double)GetValue(LabelMaxHeightProperty);
+            set => SetValue(LabelMaxHeightProperty, value);
+        }
+
+        /// <summary>
+        /// Identifies the <see cref="LabelMaxHeight"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty LabelMaxHeightProperty =
+            DependencyProperty.Register(nameof(LabelMaxHeight), typeof(double), typeof(ComboBoxPlus),
+                new FrameworkPropertyMetadata(double.PositiveInfinity),
+                new ValidateValueCallback(Util.IsMaxWidthHeightValid));
+
+        /// <summary>
+        /// Gets or sets the margin of the label.
+        /// </summary>
+        public Thickness LabelMargin
+        {
+            get => (Thickness)GetValue(LabelMarginProperty);
+            set => SetValue(LabelMarginProperty, value);
+        }
+
+        /// <summary>
+        /// Identifies the <see cref="LabelMargin"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty LabelMarginProperty =
+            DependencyProperty.Register(nameof(LabelMargin), typeof(Thickness), typeof(ComboBoxPlus));
+
+        /// <summary>
+        /// Gets or sets the vertical alignment of the label.
+        /// </summary>
+        public VerticalAlignment LabelVerticalAlignment
+        {
+            get => (VerticalAlignment)GetValue(LabelVerticalAlignmentProperty);
+            set => SetValue(LabelVerticalAlignmentProperty, value);
+        }
+
+        /// <summary>
+        /// Identifies the <see cref="LabelVerticalAlignment"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty LabelVerticalAlignmentProperty =
+            DependencyProperty.Register(nameof(LabelVerticalAlignment), typeof(VerticalAlignment), typeof(ComboBoxPlus));
+
+        /// <summary>
+        /// Gets or sets the horizontal alignment of the label.
+        /// </summary>
+        public HorizontalAlignment LabelHorizontalAlignment
+        {
+            get => (HorizontalAlignment)GetValue(LabelHorizontalAlignmentProperty);
+            set => SetValue(LabelHorizontalAlignmentProperty, value);
+        }
+
+        /// <summary>
+        /// Identifies the <see cref="LabelHorizontalAlignment"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty LabelHorizontalAlignmentProperty =
+            DependencyProperty.Register(nameof(LabelHorizontalAlignment), typeof(HorizontalAlignment), typeof(ComboBoxPlus));
+
+        /// <summary>
+        /// Gets or sets the foreground brush of the label.
+        /// </summary>
+        public Brush LabelForeground
+        {
+            get => (Brush)GetValue(LabelForegroundProperty);
+            set => SetValue(LabelForegroundProperty, value);
+        }
+
+        /// <summary>
+        /// Identifies the <see cref="LabelForeground"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty LabelForegroundProperty =
+            DependencyProperty.Register(nameof(LabelForeground), typeof(Brush), typeof(ComboBoxPlus));
+
+        /// <summary>
+        /// Gets or sets the font size of the label.
+        /// </summary>
+        public double LabelFontSize
+        {
+            get => (double)GetValue(LabelFontSizeProperty);
+            set => SetValue(LabelFontSizeProperty, value);
+        }
+
+        /// <summary>
+        /// Identifies the <see cref="LabelFontSize"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty LabelFontSizeProperty =
+            DependencyProperty.Register(nameof(LabelFontSize), typeof(double), typeof(ComboBoxPlus));
+
+        /// <summary>
+        /// Gets or sets the font family of the label.
+        /// </summary>
+        public FontFamily LabelFontFamily
+        {
+            get => (FontFamily)GetValue(LabelFontFamilyProperty);
+            set => SetValue(LabelFontFamilyProperty, value);
+        }
+
+        /// <summary>
+        /// Identifies the <see cref="LabelFontFamily"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty LabelFontFamilyProperty =
+            DependencyProperty.Register(nameof(LabelFontFamily), typeof(FontFamily), typeof(ComboBoxPlus));
+
+        /// <summary>
+        /// Gets or sets the font stretch of the label.
+        /// </summary>
+        public FontStretch LabelFontStretch
+        {
+            get => (FontStretch)GetValue(LabelFontStretchProperty);
+            set => SetValue(LabelFontStretchProperty, value);
+        }
+
+        /// <summary>
+        /// Identifies the <see cref="LabelFontStretch"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty LabelFontStretchProperty =
+            DependencyProperty.Register(nameof(LabelFontStretch), typeof(FontStretch), typeof(ComboBoxPlus));
+
+        /// <summary>
+        /// Gets or sets the font style of the label.
+        /// </summary>
+        public FontStyle LabelFontStyle
+        {
+            get => (FontStyle)GetValue(LabelFontStyleProperty);
+            set => SetValue(LabelFontStyleProperty, value);
+        }
+
+        /// <summary>
+        /// Identifies the <see cref="LabelFontStyle"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty LabelFontStyleProperty =
+            DependencyProperty.Register(nameof(LabelFontStyle), typeof(FontStyle), typeof(ComboBoxPlus));
+
+        /// <summary>
+        /// Gets or sets the font weight of the label.
+        /// </summary>
+        public FontWeight LabelFontWeight
+        {
+            get => (FontWeight)GetValue(LabelFontWeightProperty);
+            set => SetValue(LabelFontWeightProperty, value);
+        }
+
+        /// <summary>
+        /// Identifies the <see cref="LabelFontWeight"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty LabelFontWeightProperty =
+            DependencyProperty.Register(nameof(LabelFontWeight), typeof(FontWeight), typeof(ComboBoxPlus));
+
+        #endregion
+
+        #region ExternalOutline
+
+        /// <summary>
+        /// Gets or sets the background brush of the external outline.
+        /// </summary>
+        public Brush ExternalBackground
+        {
+            get => (Brush)GetValue(ExternalBackgroundProperty);
+            set => SetValue(ExternalBackgroundProperty, value);
+        }
+
+        /// <summary>
+        /// Identifies the <see cref="ExternalBackground"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty ExternalBackgroundProperty =
+            DependencyProperty.Register(nameof(ExternalBackground), typeof(Brush), typeof(ComboBoxPlus));
+
+        /// <summary>
+        /// Gets or sets the border brush of the external outline.
+        /// </summary>
+        public Brush ExternalBorderBrush
+        {
+            get => (Brush)GetValue(ExternalBorderBrushProperty);
+            set => SetValue(ExternalBorderBrushProperty, value);
+        }
+
+        /// <summary>
+        /// Identifies the <see cref="ExternalBorderBrush"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty ExternalBorderBrushProperty =
+            DependencyProperty.Register(nameof(ExternalBorderBrush), typeof(Brush), typeof(ComboBoxPlus));
+
+        /// <summary>
+        /// Gets or sets the border thickness of the external outline.
+        /// </summary>
+        public Thickness ExternalBorderThickness
+        {
+            get => (Thickness)GetValue(ExternalBorderThicknessProperty);
+            set => SetValue(ExternalBorderThicknessProperty, value);
+        }
+
+        /// <summary>
+        /// Identifies the <see cref="ExternalBorderThickness"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty ExternalBorderThicknessProperty =
+            DependencyProperty.Register(nameof(ExternalBorderThickness), typeof(Thickness), typeof(ComboBoxPlus));
+
+        /// <summary>
+        /// Gets or sets the padding of the external outline.
+        /// </summary>
+        public Thickness ExternalPadding
+        {
+            get => (Thickness)GetValue(ExternalPaddingProperty);
+            set => SetValue(ExternalPaddingProperty, value);
+        }
+
+        /// <summary>
+        /// Identifies the <see cref="ExternalPadding"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty ExternalPaddingProperty =
+            DependencyProperty.Register(nameof(ExternalPadding), typeof(Thickness), typeof(ComboBoxPlus));
+
+        /// <summary>
+        /// Gets or sets the corner radius of the external outline.
+        /// </summary>
+        public CornerRadius ExternalCornerRadius
+        {
+            get => (CornerRadius)GetValue(ExternalCornerRadiusProperty);
+            set => SetValue(ExternalCornerRadiusProperty, value);
+        }
+
+        /// <summary>
+        /// Identifies the <see cref="ExternalCornerRadius"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty ExternalCornerRadiusProperty =
+            DependencyProperty.Register(nameof(ExternalCornerRadius), typeof(CornerRadius), typeof(ComboBoxPlus));
 
         #endregion
 
