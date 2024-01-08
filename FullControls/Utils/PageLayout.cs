e02d41bccb3a39c0bc2edc2d878862bae7286541
devpelux <devpelux@outlook.com>
2024-01-08 13:06:03 +0100

    Added page layouts.

---
+++ FullControls/Utils/PageLayout.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+
+namespace FullControls.Utils
+{
+    /// <summary>
+    /// Represents a page layout.
+    /// </summary>
+    public class PageLayout
+    {
+        /// <summary>
+        /// Size of the page.
+        /// </summary>
+        public Size Size { get; }
+
+        /// <summary>
+        /// Padding of the page. (Also known as margins)
+        /// </summary>
+        public Thickness Padding { get; }
+
+        /// <summary>
+        /// Column width of the page.
+        /// </summary>
+        public double ColumnWidth => Size.Width - Padding.Left - Padding.Right;
+
+        //todo add more properties.
+
+        /// <summary>
+        /// Initializes a new instance by specifying the page size and the page margins.
+        /// </summary>
+        public PageLayout(string width, string height, string leftRight, string topBottom)
+        {
+            LengthConverter converter = new();
+            double sizeWidth = (double)converter.ConvertFromInvariantString(width)!;
+            double sizeHeight = (double)converter.ConvertFromInvariantString(height)!;
+
+            double paddingLeftRight = (double)converter.ConvertFromInvariantString(leftRight)!;
+            double paddingTopBottom = (double)converter.ConvertFromInvariantString(topBottom)!;
+
+            Size = new Size(sizeWidth, sizeHeight);
+            Padding = new Thickness(paddingLeftRight, paddingTopBottom, paddingLeftRight, paddingTopBottom);
+        }
+    }
+}
---
+++ FullControls/Utils/PageSizes.cs
@@ -0,0 +1,50 @@
+﻿namespace FullControls.Utils
+{
+    /// <summary>
+    /// Contains the most common page layouts.
+    /// </summary>
+    public class PageLayouts
+    {
+        /// <summary>
+        /// A3 Page layout.
+        /// </summary>
+        public static readonly PageLayouts A3 = new("29.7cm", "42cm", "3.18cm", "2.54cm", "1.9cm", "2.54cm", "1.27cm", "1.27cm");
+
+        /// <summary>
+        /// A4 Page layout.
+        /// </summary>
+        public static readonly PageLayouts A4 = new("21cm", "29.7cm", "3.18cm", "2.54cm", "1.9cm", "2.54cm", "1.27cm", "1.27cm");
+
+        /// <summary>
+        /// A5 Page layout.
+        /// </summary>
+        public static readonly PageLayouts A5 = new("14.8cm", "21cm", "3.18cm", "2.54cm", "1.9cm", "2.54cm", "1.27cm", "1.27cm");
+
+        //todo add more layouts.
+
+        /// <summary>
+        /// Page layout with large margins.
+        /// </summary>
+        public PageLayout MarginsLarge { get; }
+
+        /// <summary>
+        /// Page layout with mid margins.
+        /// </summary>
+        public PageLayout MarginsMid { get; }
+
+        /// <summary>
+        /// Page layout with narrow margins.
+        /// </summary>
+        public PageLayout MarginsNarrow { get; }
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        private PageLayouts(string w, string h, string largeLR, string largeTB, string midLR, string midTB, string narrowLR, string narrowTB)
+        {
+            MarginsLarge = new PageLayout(w, h, largeLR, largeTB);
+            MarginsMid = new PageLayout(w, h, midLR, midTB);
+            MarginsNarrow = new PageLayout(w, h, narrowLR, narrowTB);
+        }
+    }
+}
